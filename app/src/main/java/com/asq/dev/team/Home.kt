f918cac5f1a1be5c4dd921344936bae84854c048
aksh51n9h <42872043+aksh51n9h@users.noreply.github.com>
2018-09-23 20:56:55 +0530

    basic setup finished

--- app/src/main/java/com/asq/dev/team/Home.kt
+++ app/src/main/java/com/asq/dev/team/Home.kt
@@ -1,6 +1,6 @@
 package com.asq.dev.team
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Home : AppCompatActivity() {
